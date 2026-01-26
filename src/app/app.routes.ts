7bc260404a5ea8e3d9f4b9ffd4572fad2d941590
Mauricio Esperon <esperonmau@gmail.com>
2026-01-26 14:03:00 -0600

    auth-main layouts works - ready to work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,20 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './pages/login.component/login.component';
+import {AuthLayoutComponent} from './layouts/auth-layout.component/auth-layout.component';
+import {MainLayoutComponent} from './layouts/main-layout.component/main-layout.component';
+import {DashboardComponent} from './pages/dashboard.component/dashboard.component';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent }
+    ]
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{ path: 'dashboard', component: DashboardComponent}]
+  }
 ];
