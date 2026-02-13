9386472bc2fe5e5e073318ffdb5043c2a1f70426
Mauricio Esperon <esperonmau@gmail.com>
2026-02-13 16:31:32 -0600

    modal delete-patient done

--- src/app/pages/dashboard.component/patients.component/patient-list.component/patient-list.component.ts
+++ src/app/pages/dashboard.component/patients.component/patient-list.component/patient-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {AddModalComponent} from './modals/add-modal.component/add-modal.component';
 import {PatientEditModal} from './modals/patient-edit-modal/patient-edit-modal';
+import {PatientDeleteModal} from './modals/patient-delete-modal/patient-delete-modal';
 
 @Component({
   selector: 'app-patient-list.component',
   imports: [
     AddModalComponent,
-    PatientEditModal
+    PatientEditModal,
+    PatientDeleteModal
   ],
   templateUrl: './patient-list.component.html',
   styleUrl: './patient-list.component.css',
@@ -14,20 +16,26 @@ import {PatientEditModal} from './modals/patient-edit-modal/patient-edit-modal';
 export class PatientListComponent {
   showAddPatientModal = false;
   showEditPatientModal = false;
+  showDeletePatientModal = false;
 
   openNewPatient() {
     this.showAddPatientModal = true;
     console.log(this.showAddPatientModal);
   }
 
-  closeModal() {
-    this.showAddPatientModal = false;
-    this.showEditPatientModal = false;
-  }
-
   openEditPatientModal() {
     this.showEditPatientModal = true;
     console.log(this.showEditPatientModal);
+  }
+
+  openDeletePatientModal() {
+    this.showDeletePatientModal = true;
+  }
+
+  closeModal() {
+    this.showAddPatientModal = false;
+    this.showEditPatientModal = false;
+    this.showDeletePatientModal = false;
   }
 
 
